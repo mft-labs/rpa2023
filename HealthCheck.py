9773579cece61023dbfe278b679848a1138ef30c
madhu72 <madhu72@users.noreply.github.com>
2023-07-24 22:22:19 +0530

    Get Route

--- HealthCheck.py
+++ HealthCheck.py
@@ -1,6 +1,7 @@
 from robot.api.deco import keyword, library
 from datetime import datetime
-
+import os
+import requests
 
 @library
 class HealthCheck:
@@ -10,6 +11,11 @@ class HealthCheck:
     def get_date_time(self):
         now = datetime.now()
         date_time = now.strftime("%Y%m%d%H%M%S")
+        return date_time
+
+    def get_date_time2(self):
+        now = datetime.now()
+        date_time = now.strftime("%Y-%m-%d %H:%M:%S")
         return date_time
 
     def add_datetime(self, filename):
@@ -50,35 +56,29 @@ class HealthCheck:
         f1.close()
         return '\nProcess of GET POD Completed'
 
-
-
-
-    @keyword('Get Pod')
+    @keyword('Get Route')
     def get_route(self, text):
+        logfile2 = self.logname
+        base = os.path.basename(self.logname)
+        logfile = 'get_route_'+os.path.basename(self.logname)
+        logfile = logfile2.replace(base, logfile)
         headers, data = self.prepare_data(text)
-        name = headers[0]
+        index = headers.index('PORT')
+        errorlog = open(logfile, 'w')
+        for rec in data:
+            fields = rec.split()
+            if fields[index] == 'https':
+                label = fields[0]
+                url = fields[index]+'://'+str(fields[1])+str(fields[2])
+                resp = requests.get(url)
+                if resp.status_code != 200:
+                    errorlog.write('{:20s} Name: {:40s} Url: {:60s} not reachable, status code {}'.format(self.get_date_time2(),label,url, resp.status_code))
+        errorlog.close()
 
 
 
 
-    @keyword('Process Data')
-    def parse_data(self, details, headers, data):
-        f = open(self.logname, 'w')
-        f1 = open(self.logname+".err", 'w')
-        f2 = open(self.logname+'.rpt', 'w')
-        f2.write(details)
-        f2.close()
-        #f.write(str(headers))
-        f.write('{:50s} {:20s} {:20s}'.format(headers[0], headers[2], headers[3]))
-        f.write('\n')
-        for elem in data:
-            # f.write(str(elem))
-            item = elem.split()
-            f.write('{:50s} {:20s} {:20s}'.format(item[0], item[2], item[3]))
-            if item[2].find('CrashLoopBackOff') != -1 or item[2].find('Error') != -1:
-                f1.write('Error occurred for {:50s}'.format(item[0]))
-                f1.write('\n')
-            f.write('\n')
-        f.close()
-        f1.close()
-        return '\nParsing of data completed'
+
+
+
+
--- healthcheck.robot
+++ healthcheck.robot
@@ -1,19 +1,37 @@
 *** Settings ***
 Library     OperatingSystem
 Library     String
-Library     ./HealthCheck.py     ${LOGNAME}
+Library     ./HealthCheck.py    ${LOGNAME}
+Library     RequestsLibrary
+Library     StringFormat
 
 *** Test Cases ***
 
 Get Pod
-    ${text}=        Run     oc get pod
+    ${text}=        Run         oc get pod
     ${result}=      Get Pod     ${text}
     Log To Console  ${result}
     Log     ${text}
 
 Get Route
-    ${text}=        Run     oc get route
-    ${result}=      Get Route   ${text}
-    Log To Console  ${result}
-    Log     ${text}
+    ${text}=        Run                 oc get route
+    #${result}=      Get Route       ${text}
+    ${lines}=       Split To Lines      ${text}     1
+    ${header}=      Get Line            ${text}     0
+    @{headers}=     Split String        ${header}
+    FOR     ${line}     IN      @{lines}
+        @{flds}=        Split String    ${line}
+        Log To Console      ${flds}
+        IF  ${flds}[4] == 'https'
+            IF ${flds}[2] != ''
+                ${url}=     Format String       https://{0}/{1}      ${flds}[1]     ${flds}[2]
+                ${response}=    GET     ${url}
+                Status Should Be    200
+            ELSE
+                ${url}=     Format String       https://{0}     ${flds}[1]
+                ${response}=    GET     ${url}
+                Status Should Be    200
+            END
+        END
+    END
 
