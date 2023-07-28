ae09f28d65a0c207b529aa55bbb6ed7e624e67dd
madhu72 <madhu72@users.noreply.github.com>
2023-07-28 21:05:03 +0530

    add variables

---
+++ variables/oclogin.py
@@ -0,0 +1,4 @@
+OC_LOGIN_CMD="oc login --token=sha256~deb-o2uNBIJoSEgrjbDt2Ha861jbCMI0oOs8rKdl8CU --server=https://api.sandbox-m2.ll9k.p1.openshiftapps.com:6443"
+#OC_LOGIN_CMD="oc login -s=LoginUrl -u=Username -p=Passwd"
+OC_LOGIN_TYPE="token"
+OC_LOGIN_PASS=""
